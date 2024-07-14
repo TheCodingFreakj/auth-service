0cb42117fbf014931c39277999d4b34c479b1130
pallavy57 <priyadarshinipallavi18@gmail.com>
2024-07-14 21:56:47 +0530

    addd logs and view

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('protected/', ProtectedView.as_view(), name='protected'),
-    path('user-info/', UserInfoView.as_view(), name='user-info'),
+    path('userinfo/', UserInfoView.as_view(), name='userinfo'),
 ]
